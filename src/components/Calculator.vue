45f8aae741ea9d4bd2e43d7452b11aa4099eda46
Khang Vo <khangvo88@gmail.com>
2021-11-06 18:40:35 -0500

    [f] enable quit action for global Terminal

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,8 +1,22 @@
-<script setup>
+<script setup lang="ts">
 import Terminal from "./Terminal.vue";
 import { ref } from "vue";
+import { KEYCODE_D, KEYCODE_Q } from "../constants";
 
 let terminalEnabled = ref(true);
+
+const keydownHandler = (e: {
+  key: string;
+  keyCode: number;
+  ctrlKey: boolean;
+}): void => {
+  if (
+    terminalEnabled &&
+    (e.keyCode === KEYCODE_Q || (e.keyCode === KEYCODE_D && e.ctrlKey))
+  ) {
+    terminalEnabled.value = false;
+  }
+};
 </script>
 
 <template>
@@ -15,9 +29,11 @@ let terminalEnabled = ref(true);
 
   <Terminal
     v-if="terminalEnabled"
+    tabindex="-1"
     class="terminal"
     title="CLI RPN Calculator"
     @close-terminal="terminalEnabled = false"
+    @keydown="keydownHandler"
   />
 </template>
 
